87fae263ca57be7f8ec0f4a2d64515506dd5a071
A-Nony-Mus <hovcerblade@gmail.com>
2018-10-31 20:10:20 -0700

    Update celsius:faranheit.c

--- celsius:faranheit.c
+++ celsius:faranheit.c
@@ -1,85 +1,74 @@
 /*A-Nony-Mus
  13 Feb. 2012
- this program converts between degrees celsius, faranheit, and kelvin based on user input*/
+ this program converts between degrees celsius, faranheit, and kelvin based on user input
+ 18 Feb. 2012
+ rewrote the if statements in the main loop to a switch statement, program now outputs corrects values,
+ added a while statement to loop the process
+ 27 Feb. changed the values in the switch statement to characters instead of numbers
+ 10 Mar. 2012
+ rewrote conversion function to use pointers and return all converted values with one call and the main function to reflect that*/
 
 #include <stdio.h>
 #define KELVIN_NUMBER 273.15
 
 //function prototypes
-double conversion(double x, char y);
+void conversion(double x, char y, double *a, double *b, double *c);
 
 int
 main(void)
 {
-
-char input,
-    k_input;
     
-double int_temp,
-    converted_temp,
-    temp3;
-
-k_input = 99;
+    char input;         //temperature unit
     
-//gather the necessary inputs from the user
-printf("would you like to convert celsius (c), faranheit (f), or kelvin (k)? ");
-scanf("%c", &input);
-printf("what temperature would you like to convert? ");
-scanf("%lf", &int_temp);
-    
+    double int_temp,    //initial input temperature
+    celsius,            //this is where the converted celsius temperatue is stored
+    fahrenheit,         //this is where the converted fahrenheit temperatue is stored
+    kelvin,             //this is where the converted kelvin temperature is stored
+    yesno;              //loop control variable
 
-//how to calculate the temperatures if the input is kelvin
-if (input == 107)
-{
-converted_temp = int_temp - KELVIN_NUMBER;
-temp3 = conversion(converted_temp, k_input);
-}
-else if (input == 99 || 102)//how to calculate the temperatures if the input is celsius or faranheit
-{
-converted_temp = conversion(int_temp, input);
-    if (input == 99) {
-        temp3 = int_temp + KELVIN_NUMBER;
+    yesno = 1;
+while (yesno == 1){ 
+    //gather the necessary input from the user
+    printf("Please input the temperature you would like to convert followed by a space then the first letter of the scale it is in (ex: 107 c) ");
+    scanf("%lf %c", &int_temp, &input);
+    
+    //convert the input and print the results
+    conversion(int_temp, input, &celsius, &fahrenheit, &kelvin);
+    if(input = 'c'
+       || 'f'
+       || 'k')
+        printf("The converted temperatures are:\n in Celsius: %9.2f\n in Fahrenheit: %6.2f\n in Kelvin %11.2f\n", celsius, fahrenheit, kelvin);
+
+    //loop?
+    printf("would you like to continue (1=yes, 2=no)? ");
+    scanf("%lf", &yesno);    
     }
-    else
-        temp3 = converted_temp + KELVIN_NUMBER;
-}
-
-//display the temperatures
-if (input == 99) //if the original input was celsius
-{
-    printf("your temperature, in\n");
-    printf("degrees celsius is %9.2f\ndegrees farenheit is %7.2f\ndegrees kelvin is %10.2f\n", int_temp, converted_temp, temp3);
-}
-else if (input == 102) //if the original input was faranheit
-{
-
-    printf("your temperature, in\n");
-    printf("degrees farenheit is %7.2f\ndegrees celsius is %9.2f\ndegrees kelvin is %10.2f\n", int_temp, converted_temp, temp3);
-}
-else if (input == 107)
-{
-    printf("your temperature, in\n");
-    printf("degrees kelvin is %10.2f\ndegrees celsius is %9.2f\ndegrees faranheit is %7.2f\n", int_temp, converted_temp, temp3);
-}
-else
-printf("I'm sorry, that was not one of the inputs\n");
-
-
-return(0);
+    
+    return(0);
 }
 
 /*Inputs: the input number and the character to determine whether it is faranheit or celsius
-Output: the temperature converted to the other unit of measurement*/
-double conversion(double x, char y)
+ Output: all converted temperatures as *a, *b, and *c*/
+void conversion(double x, char y, double *a, double *b, double *c)
 {
-    double temp;
-    
-    if (y == 99) //celsius to faranheit
-        temp = ((x * 9)/5) + 32;
-    else if (y == 102) //faranheit to celsius
-        temp = ((x - 32) * 5) / 9;
-    else if (y == 107)
-        temp = x - KELVIN_NUMBER;
-
-return(temp);
+    switch (y) {
+        case 'c':
+            *a = x;
+            *b = ((x * 9)/5)+32;
+            *c = x + KELVIN_NUMBER;
+            break;
+        case 'f':
+            *a = ((x - 32)*5)/9;
+            *b = x;
+            *c = *a + KELVIN_NUMBER;
+            break;
+        case 'k':
+            *a = x - KELVIN_NUMBER;
+            *b = ((*a * 9)/5)+32;
+            *c = x;
+            break;
+        default:
+            printf("I'm sorry, that was not one of the inputs\n");
+            break;
+    }
 }
